fbd8dca50b451a238c43684a4b24a307b6f0472c
Timas Jackus <jackustimas@gmail.com>
2018-02-22 09:43:32 +0200

    C budas su Masyvu & Fixai su vektoriais

--- duomenu-apdorojimas_Masyvai.cpp
+++ duomenu-apdorojimas_Masyvai.cpp
@@ -8,7 +8,6 @@
 int randPazymys();
 
 int main() {
-  std::vector<int> namu_darbai;
   std::string vardas, pavarde;
   int n, egz, ivestis, ifRandom;
   double vidurkis = 0;
@@ -27,30 +26,29 @@ int main() {
   }
   std::cout << "Iveskite pazymiu skaiciu (n): ";
   std::cin >> n;
+  int *namu_darbai = new int [n];
   if(ifRandom == 0) {
     for (int i = 0; i < n; i++) {
-      int input;
-      std::cin >> input;
-      namu_darbai.push_back(input);
+      std::cin >> namu_darbai[i];
     }
   } else {
     for (int i = 0; i < n; i++)
-      namu_darbai.push_back(randPazymys());
+      namu_darbai[i] = randPazymys();
   }
   std::cout << "Pasirinkite skaiciavimo buda (Mediana - 0, Vidurkis - 1): ";
   std::cin >> ivestis;
   std::cout << "Studento Vardas Pavarde: " << vardas << " " << pavarde << std::endl;
-  std::sort(namu_darbai.begin(), namu_darbai.end());
+  std::sort(namu_darbai, namu_darbai + n);
   std::cout << "Studento namu darbu pazymiai: ";
-  for(auto const& value: namu_darbai) {
-      std::cout << value << " ";
-      vidurkis += value;
+  for(int i = 0; i < n; i++) {
+      std::cout << namu_darbai[i] << " ";
+      vidurkis += namu_darbai[i];
   }
   std::cout << std::endl;
-  if (namu_darbai.size() % 2 == 0)
-    mediana = (namu_darbai[namu_darbai.size() / 2] + namu_darbai[(namu_darbai.size() / 2) - 1]) / 2.0;
+  if (n % 2 == 0)
+    mediana = (namu_darbai[n / 2] + namu_darbai[(n / 2) - 1]) / 2.0;
   else
-    mediana = namu_darbai[(namu_darbai.size() / 2)];
+    mediana = namu_darbai[(n / 2)];
   vidurkis = (double) vidurkis / n;
   std::cout << "Egzamino rezultatai: " << egz << std::endl;
   std::cout << std::setprecision(2) << std::fixed << "Galutinis balas: " << 0.4 * (ivestis == 0 ? mediana : vidurkis) + 0.6 * egz << std::endl;
